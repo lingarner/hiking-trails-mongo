b594497053b8b9be193ee9e530c14280417d27df
Lindsay Garner <gar21034@byui.edu>
2023-10-12 14:51:46 -0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = new express.Router();
+const swagger = require('./swagger.js')
 const controller = require('../controller/controller.js');
+
+router.use("/", swagger)
 
 router.get('/', controller.getTrails)
 router.post('/', controller.addTrail);
