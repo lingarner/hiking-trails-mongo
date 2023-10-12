0659639b657110df3ded3ca35db56c0a39e2946d
Lindsay Garner <gar21034@byui.edu>
2023-10-12 15:07:11 -0600

    revert to og uri

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors())
 app.use(bodyParser.json())
-
 app.use('/', require('./routes/index.js') )
 
 
